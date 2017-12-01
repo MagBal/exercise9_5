1677a3a5cad406ef0462d8e49b61eaf18dd70a4c
Magda Balun <magi@op.pl>
2017-12-01 22:57:00 +0100

    Add index.html and scripts.js files

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var myButtonsClass = document.getElementsByClassName('button');
+
+console.log(myButtonsClass);
+
+for (var i = 0; i < myButtonsClass.length; i++) {
+	console.log(myButtonsClass[i].innerText);
+}
+
+
+
+
+
